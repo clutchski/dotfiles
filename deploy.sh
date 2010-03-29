1ef03e8f33d83a693e543ac04414ba9e6ebb6d69
clutchski <clutchski@gmail.com>
2010-03-29 11:33:05 -0400

    seperating config reads

--- deploy.sh
+++ deploy.sh
@@ -3,20 +3,18 @@
 set -e
 
 # template git profile
-
-echo
-echo "enter your global git profile: your@email.com Your Name"
-read email name
+echo "enter your global git user name:"
+read name
+echo "enter your global git e-mail:"
+read email
 
 sed -e "s|#NAME#|$name|" -e "s|#EMAIL#|$email|" gitconfig.template > gitconfig
 
 # deploy the files
-
 for f in screenrc vimrc bash_aliases ackrc gitconfig gitignore
 do
     cp $f ~/.${f}
 done
 
 # clean-up
-
 rm gitconfig
