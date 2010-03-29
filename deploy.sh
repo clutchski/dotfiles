0ce89e9832f8695422e437caf7f4b23e27c1c940
clutchski <clutchski@gmail.com>
2010-03-29 11:23:43 -0400

    templating global git username/e-mail

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
+set -e
+
+# template git profile
+
+cp gitconfig.template gitconfig
+echo
+echo "enter your global git profile: your@email.com Your Name"
+read email name
+
+sed -i -e "s|#NAME#|$name|" -e "s|#EMAIL#|$email|" gitconfig
+
+# deploy the files
+
 for f in screenrc vimrc bash_aliases ackrc gitconfig gitignore
 do
     cp $f ~/.${f}
 done
+
+# clean-up
+
+rm gitconfig
