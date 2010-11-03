172deb59d5dbbcf07dd869da481d5ed9aa0768f0
clutchski <clutchski@gmail.com>
2010-11-02 21:52:44 -0400

    Adding config for global gitignore.

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ read name
 echo "enter your global git e-mail:"
 read email
 
-sed -e "s|#NAME#|$name|" -e "s|#EMAIL#|$email|" gitconfig.template > gitconfig
+sed -e "s|#HOME|$HOME|" -e "s|#NAME#|$name|" -e "s|#EMAIL#|$email|" gitconfig.template > gitconfig
 
 # deploy
 for f in screenrc vimrc bash_aliases ackrc gitconfig gitignore pythonrc
@@ -20,6 +20,13 @@ mkdir -p ~/.vim
 cp -r vim/* ~/.vim
 
 mkdir -p ~/bin
+
+which git &> /dev/null
+if [ $? -eq 0 ]
+then
+    git config --global core.excludesfile ~/.gitignore
+fi
+
 
 # clean-up
 rm gitconfig
