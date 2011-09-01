9fed2109b8709e637700839dedb888eb19f3d64d
clutchski <clutchski@gmail.com>
2011-09-01 00:14:57 -0400

    Simplifying deployment.

--- deploy.sh
+++ deploy.sh
@@ -2,25 +2,21 @@
 
 set -e
 
-# template git profile
-echo "enter your global git user name:"
-read name
-echo "enter your global git e-mail:"
-read email
-
-sed -e "s|#HOME#|$HOME|" -e "s|#NAME#|$name|" -e "s|#EMAIL#|$email|" gitconfig.template > gitconfig
-
 # deploy
-for f in screenrc vimrc bash_aliases ackrc gitconfig gitignore pythonrc z.sh gntrc gemrc tmux.conf zshrc
+for f in `ls . `
 do
-    cp $f ~/.${f}
+    if [[ $f == "README" ]] || [[ $f == "deploy.sh" ]] ; then
+        continue
+    fi
+
+    if [ -d $f ]; then
+        mkdir -p ~/.${f}
+        cp -r ${f}/* ~/.${f}
+    else
+        cp $f ~/.${f}
+    fi
+
 done
 
-for d in vim bin oh-my-zsh
-do
-    mkdir -p ~/.${d}
-    cp -r ${d}/* ~/.${d}
-done
-
-# clean-up
-rm gitconfig
+# git doesn't seem to like relative paths in here.
+sed -i -e "s|#HOME#|$HOME|" ~/.gitconfig
