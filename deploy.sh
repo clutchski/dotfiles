93b623131bd08b33728adbef39ee6a53e206a21c
clutchski <clutchski@gmail.com>
2011-09-06 12:57:52 -0400

    Cleaning up gitconfig.

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,10 @@
 
 set -e
 
+#
 # deploy
+#
+
 for f in `ls . `
 do
     if [[ $f == "README" ]] || [[ $f == "deploy.sh" ]] ; then
@@ -15,8 +18,15 @@ do
     else
         cp $f ~/.${f}
     fi
-
 done
 
-# git doesn't seem to like relative paths in here.
-sed -i -e "s|#HOME#|$HOME|" ~/.gitconfig
+#
+# update git config
+#
+
+git config --global core.excludesfile ~/.gitignore
+
+if [[ -n $1  &&  -n $2 ]] ; then
+    git config --global user.name "$1"
+    git config --global user.email "$2"
+fi
