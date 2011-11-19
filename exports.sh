5b8ba3b05b3d729fb06f36447f55cad999e6a12c
clutchski <clutchski@gmail.com>
2011-11-19 14:36:49 -0500

    Load tmuxinator if installed.

--- exports.sh
+++ exports.sh
@@ -8,5 +8,7 @@ export HISTFILE=~/.histfile
 export HISTSIZE=1000
 export SAVEHIST=1000
 
-# If RVM is installed, use it.
+# Load shell plugins.
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
+[[ -s "$HOME/.tmuxinator/scripts/tmuxinator" ]] && source "$HOME/.tmuxinator/scripts/tmuxinator"
+
