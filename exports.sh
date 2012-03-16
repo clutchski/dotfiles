d2ba34a6b51c839b22258def58131daecb3f43d6
clutchski <clutchski@gmail.com>
2012-03-16 14:57:01 -0400

    Disable virtual env prompt.

--- exports.sh
+++ exports.sh
@@ -7,6 +7,7 @@ export PATH=$PATH:~/bin
 export HISTFILE=~/.histfile
 export HISTSIZE=1000
 export SAVEHIST=1000
+export VIRTUAL_ENV_DISABLE_PROMPT=true
 
 # Load shell plugins.
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
