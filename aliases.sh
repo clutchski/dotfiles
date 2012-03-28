0e82397d72dff1b3c74b4f302bd25bd39355edaf
clutchski <clutchski@gmail.com>
2012-03-28 12:38:51 -0400

    open ubuntu alias.

--- aliases.sh
+++ aliases.sh
@@ -2,12 +2,17 @@
 alias du="du -h"
 alias df="df -h"
 
-# ack = ack-grep on ubuntu
-which ack-grep &> /dev/null
-if [ $? -eq 0 ]
-then
-    alias ack="ack-grep"
-fi
+alias_if_exists () {
+    which $1 &> /dev/null
+    if [ $? -eq 0 ]
+    then
+        alias $2="$1"
+    fi
+}
+
+# Ubuntu aliases.
+alias_if_exists "ack-grep" "ack"
+alias_if_exists "gnome-open" "open"
 
 # ack shortcuts
 alias acka="ack --all"
