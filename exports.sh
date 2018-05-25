3941ee9209086a68283cfc487edfebc26c7eb7f6
Matt Perpick <matthewperpick@gmail.com>
2018-05-25 16:35:22 -0400

    /usr/local/bin

--- exports.sh
+++ exports.sh
@@ -3,7 +3,7 @@
 export PYTHONSTARTUP=~/.pythonrc
 export EDITOR=vim
 export SCREENDIR=~/.screen
-export PATH=$PATH:~/bin
+export PATH=$PATH:/usr/local/bin:~/bin
 export HISTFILE=~/.histfile
 export HISTSIZE=1000
 export SAVEHIST=1000
