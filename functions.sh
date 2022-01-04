86ba5de9907f0acff2d261575ec5b5e60c70c6f4
Matt Perpick <matthewperpick@gmail.com>
2022-01-04 15:06:59 -0500

    block timewasting websites

--- functions.sh
+++ functions.sh
@@ -44,3 +44,33 @@ camera_reset() {
 youtube_to_mp3() {
     youtube-dl --extract-audio --audio-format mp3 $1:x
 }
+
+etchost_block () {
+  if [[ $# -eq 0 ]]; then
+    # no args == show list of already-blocked hosts
+    grep --color=never --ignore-case --extended-regexp "^##\s+block" /etc/hosts | cut -d" " -f 3-
+  else
+    # loop through hosts and block each one on ipv4 + ipv6
+    while (($#)); do
+      echo "echo \"\n## block $1\"        >> /etc/hosts" | sudo sh
+      echo "echo \"127.0.0.1  $1 www.$1\" >> /etc/hosts" | sudo sh
+      echo "echo \"::1        $1 www.$1\" >> /etc/hosts" | sudo sh
+      echo "echo \"## /block $1\"         >> /etc/hosts" | sudo sh
+      shift
+    done
+  fi
+}
+
+etchost_unblock () {
+  while (($#)); do
+    # remove specified hosts and squash multiple empty lines
+    newContents=$(sed "/## block $1/,/## \/block/d" /etc/hosts | sed '/./,/^$/!d')
+    echo "echo \"$newContents\" > /etc/hosts" | sudo sh
+    shift
+  done
+}
+
+
+etchost_block_blackholes() {
+    etchost_block facebook.com reddit.com
+}
