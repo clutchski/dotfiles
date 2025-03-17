4b749b12551b6e5d87c359ec7dd00eaaffe002d2
Matt Perpick <matthewperpick@gmail.com>
2025-03-17 14:43:01 -0700

    Add rust, asdf, etc.

--- exports.sh
+++ exports.sh
@@ -3,17 +3,22 @@
 export PYTHONSTARTUP=~/.pythonrc
 export EDITOR=vim
 export SCREENDIR=~/.screen
-export PATH=$PATH:/usr/local/bin:~/bin:/opt/homebrew/bin:/usr/local/go/bin/
 export HISTFILE=~/.histfile
 export HISTSIZE=1000
 export SAVEHIST=1000
 export VIRTUAL_ENV_DISABLE_PROMPT=true
+export PATH=$PATH:/usr/local/bin:~/bin:/opt/homebrew/bin:/usr/local/go/bin/
+
+# install asdf
+export PATH="$PATH:$HOME/.asdf/shims"
 
 # install pyenv
 export PATH="$HOME/.pyenv/bin:$PATH"
 eval "$(pyenv init --path)"
 eval "$(pyenv init -)"
 
+# install rust
+export PATH=$PATH:~/.cargo/bin
 
 # Load shell plugins.
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
--- homebrew.sh
+++ homebrew.sh
@@ -3,5 +3,5 @@ brew install ripgrep
 brew install the_silver_searcher
 brew install wget
 brew install zsh
-brew install ffmpeg
-brew install yt-dlp
+# brew install ffmpeg
+# brew install yt-dlp
