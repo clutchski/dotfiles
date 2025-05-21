a44d5d54317312f0b64e34156fdce22f4b9c2c6f
Matt Perpick <matthewperpick@gmail.com>
2025-05-21 15:28:29 -0400

    blackhole

--- functions.sh
+++ functions.sh
@@ -2,12 +2,14 @@
 
 
 . ~/.exts/git.sh
+. ~/.exts/blackhole.sh
+
 
 # Run claude from the repository root
 claude-root() {
   local root_dir
   root_dir=$(git rev-parse --show-toplevel 2>/dev/null)
-  
+
   if [ $? -eq 0 ]; then
     cd "$root_dir" && claude "$@"
   else
@@ -44,40 +46,6 @@ camera_reset() {
 
 youtube_to_mp3() {
     yt-dlp --extract-audio --audio-format mp3 $1:x
-}
-
-etchost_block () {
-  if [[ $# -eq 0 ]]; then
-    # no args == show list of already-blocked hosts
-    grep --color=never --ignore-case --extended-regexp "^##\s+block" /etc/hosts | cut -d" " -f 3-
-  else
-    # loop through hosts and block each one on ipv4 + ipv6
-    while (($#)); do
-      if ! grep -q "## block $1" /etc/hosts; then
-        echo "echo \"\n## block $1\"        >> /etc/hosts" | sudo sh
-        echo "echo \"127.0.0.1  $1 www.$1\" >> /etc/hosts" | sudo sh
-        echo "echo \"::1        $1 www.$1\" >> /etc/hosts" | sudo sh
-        echo "echo \"## /block $1\"         >> /etc/hosts" | sudo sh
-      fi
-      shift
-    done
-  fi
-}
-
-etchost_unblock () {
-  while (($#)); do
-    # remove specified hosts and squash multiple empty lines
-    newContents=$(sed "/## block $1/,/## \/block/d" /etc/hosts | sed '/./,/^$/!d')
-    echo "echo \"$newContents\" > /etc/hosts" | sudo sh
-    shift
-  done
-}
-
-
-etchost_block_blackholes() {
-    while read b; do
-        etchost_block $b
-    done < ~/.blackholes
 }
 
 op_save_login() {
