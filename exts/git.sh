bd99fd5fe6e9368c3df35c16e260d5dd1fb923ab
Matt Perpick <matthewperpick@gmail.com>
2025-11-07 11:58:05 -0500

    short worktree names

--- exts/git.sh
+++ exts/git.sh
@@ -92,7 +92,7 @@ git_worktree_create() {
   SANITIZED_BRANCH=$(echo "$BRANCH_NAME" | tr '/' '-')
 
   # Create worktree directory name
-  WORKTREE_DIR="${PARENT_DIR}/${REPO_NAME}_worktree_${SANITIZED_BRANCH}"
+  WORKTREE_DIR="${PARENT_DIR}/w_${REPO_NAME}_${SANITIZED_BRANCH}"
 
   if [ -d "$WORKTREE_DIR" ]; then
     echo "Error: Directory already exists: $WORKTREE_DIR"
