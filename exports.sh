4179d248e2f2a35d7648ab319888e0d1d5ebf625
Matt Perpick <matthewperpick@gmail.com>
2026-01-02 12:05:44 -0500

    changes

--- exports.sh
+++ exports.sh
@@ -10,4 +10,4 @@ export VIRTUAL_ENV_DISABLE_PROMPT=true
 
 export PNPM_HOME="$HOME/Library/pnpm"
 
-export PATH=$PATH:/usr/local/bin:~/bin:/opt/homebrew/bin:/usr/local/go/bin/:~/.cargo/bin:$PNPM_HOME
+export PATH=$PATH:/usr/local/bin:~/bin:/opt/homebrew/bin:/usr/local/go/bin/:~/.cargo/bin:$PNPM_HOME:~/.local/bin
