bebd37a9941b56b5d29996406fe663ff565752fc
Matt Perpick <matthewperpick@gmail.com>
2026-02-03 19:36:18 -0500

    bump

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,25 +5,29 @@ REPO="https://github.com/clutchski/dotfiles"
 DOTFILES_DIR="$HOME/.dotfiles"
 
 echo ""
+echo "    .___      __    __  .__        "
+echo "  __| _/_____/  |__/  |_|__| ____  "
+echo " / __ |/  _ \   __\   __\  |/ __ \ "
+echo "/ /_/ (  <_> )  |  |  | |  \  ___/ "
+echo "\____ |\____/|__|  |__| |__|\___  >"
+echo "     \/                         \/ "
+echo ""
+echo "        Bootstrapping your dotfiles..."
+echo ""
+
 echo "==> Cloning dotfiles"
 echo ""
 echo "    Repository: $REPO"
 echo "    Target:     $DOTFILES_DIR"
-echo ""
 
 if [ -d "$DOTFILES_DIR" ]; then
-    echo "    Pulling latest..."
     git -C "$DOTFILES_DIR" pull --quiet
 else
-    echo "    Cloning..."
     git clone --quiet "$REPO" "$DOTFILES_DIR"
 fi
 
-echo "    Done"
-
 curl -fsSL https://raw.githubusercontent.com/clutchski/dottie/main/scripts/install.sh | bash
 
-echo ""
 echo "==> Running dottie"
 echo ""
 
