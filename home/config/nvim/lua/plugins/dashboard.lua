0de941b0a97162ae60bf42b581c678ccd5c83a0c
Matt Perpick <matthewperpick@gmail.com>
2026-02-10 12:59:05 -0600

    don't show git files

--- home/config/nvim/lua/plugins/dashboard.lua
+++ home/config/nvim/lua/plugins/dashboard.lua
@@ -6,7 +6,14 @@ return {
             sections = {
                 { section = "header" },
                 { title = "Recent Files", padding = 1 },
-                { section = "recent_files", limit = 5, padding = 1 },
+                {
+                    section = "recent_files",
+                    limit = 5,
+                    padding = 1,
+                    filter = function(file)
+                        return not file:find("/.git/", 1, true)
+                    end,
+                },
                 { section = "keys", gap = 1 },
             },
             preset = {
